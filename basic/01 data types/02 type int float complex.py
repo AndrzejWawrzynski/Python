b14e5c44fd65dabb985720c81fe68cfd1602640d
Andrzej Wawrzyński <wandrzej@interia.pl>
2024-02-13 20:35:26 +0100

    13.02.20.35

---
+++ basic/01 data types/02 type int float complex.py
@@ -0,0 +1,7 @@
+numInt = 1000 # liczba całkowita
+print(numInt)
+print(type(numInt)) # <class 'int'>
+
+numFloat = 3.14 # liczba rzeczywista
+print(numFloat)
+print(type(numFloat)) # <class 'float'>

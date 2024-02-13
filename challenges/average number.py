09297a0e1e5b4b80f2c077997ecbcc8250ca0c38
Andrzej Wawrzyński <wandrzej@interia.pl>
2024-02-13 20:45:29 +0100

    average number

---
+++ challenges/average number.py
@@ -0,0 +1,10 @@
+# 1. Oblicz srednia z nastepujacych liczb: 1, 2, 3 oraz 4
+#    Przypisz obliczona wartosc do zmiennej averageNum
+#    Przy obliczeniach pamietaj by skorzystac z nawiasow
+#    dla prawidiowej kolejnosci dzialan
+# 2. Pokaz w konsoli srednia z liczb
+# 3. Pokaz równiez typ tej sredniej z funkcja type
+
+averageNumber = (1 + 2 + 3 + 4)/4
+print(averageNumber)
+print(type(averageNumber))

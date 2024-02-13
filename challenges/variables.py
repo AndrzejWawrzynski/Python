197473e2b789ec8219e994503452c4f91e33258b
Andrzej Wawrzyński <wandrzej@interia.pl>
2024-02-13 20:21:58 +0100

    13.02

---
+++ challenges/variables.py
@@ -0,0 +1,20 @@
+# Zapisz zmienne z identyfikatorami które najlepiej
+# opisuja wiadomosc email np tytut, tresc, nadawca itd.
+# Zapisz przy okazji przyktadowe wartosci w tych zmiennych
+# Wyswietl te zmienne w konsoli za pomoca funkcji print()
+
+title = "Pierwszy email"
+content = "Hello World"
+sender = "Asia Kowalska"
+receiver = "Ola"
+emailDate = "20xx.10.09"
+numAttachments = 1
+attachmentName = "file.doc"
+
+print("Tytuł emaila:" + title)
+print("Tresć emaila:", content)
+print ("Nadawca:", sender)
+print("Odbiorca:", receiver)
+print("Data emaila:", emailDate)
+print("Ilose załączników:", numAttachments)
+print("Załącznik nr. 1:", attachmentName)

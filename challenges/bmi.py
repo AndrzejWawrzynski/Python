2a7d4a9b7826bed5d5512764209efa5a0e9604f7
Andrzej Wawrzyński <wandrzej@interia.pl>
2024-02-14 19:49:27 +0100

    14.02.19.50

---
+++ challenges/bmi.py
@@ -0,0 +1,10 @@
+# Oblicz wskaznik BMI
+# 18.5 ‹ BMI < 24.9
+# wzor (wzrost w metrach np 1.7):
+# bmi = masa / (wzrost * wzrost)
+
+# Oblicz bmi wedlug powyzszego wzoru dla wagi 70
+# i wzrostu 1.75, pokaz wynik w konsoli
+
+bmi = 70/(1.75 * 1.75)
+print("BMI dla wagi 70kg i wzrostu 1,75m to : ", bmi)

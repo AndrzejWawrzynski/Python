cbb43d9ed07012cbb8eba44f8414ce2603b3d8f8
Andrzej Wawrzyński <wandrzej@interia.pl>
2024-02-15 11:35:09 +0100

    15.02.11.35

---
+++ challenges/boolen.py
@@ -0,0 +1,23 @@
+# 1. Napisz program sprawdzajacy czy liczba 10 w zmiennej
+#    num jest:
+#    - dodatnia
+#    - ujemna
+#    - równa zero
+# 2. Pokaz informacje w konsoli jaka jest to liczba
+# 3. Pamietaj o uzyciu instrukcji warunkowej if (pamietaj
+#    o wcieciu) np:
+#    if num > 4:
+#    print(num, " jest wieksze od 4")
+#
+#    if someNumber == 1：
+#        print("liczba równa jeden")
+
+num = 10
+if num == 0:
+    print(num, "rowna sie zero")
+
+if num > 0:
+    print(num, "to liczba dodatnia")
+
+if num < 0:
+    print(num, "to liczba ujemna")

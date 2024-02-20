e34152a0e3f5d42faecf4e689f7afa8ef4ba3ea9
Andrzej Wawrzyński <wandrzej@interia.pl>
2024-02-20 09:42:11 +0100

    20.02.09.40

---
+++ basic/01 data types/12 type None.py
@@ -0,0 +1,11 @@
+
+currentTaskNumber = None
+
+print(type(currentTaskNumber))
+
+if currentTaskNumber is True:
+    print("is True")
+elif currentTaskNumber is False:
+    print("is False")
+else:
+    print("is None")

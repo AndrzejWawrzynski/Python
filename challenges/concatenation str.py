b1e34ae47b6c6a1f3b2371a92c52b41825bb74d6
Andrzej Wawrzyński <wandrzej@interia.pl>
2024-02-22 13:08:20 +0100

    22.02.13.10

--- challenges/concatenation str.py
+++ challenges/concatenation str.py
@@ -4,3 +4,14 @@ title = "Dr."
 
 fullName = title + " " + firstname + " " + lastName
 print(fullName)
+
+str = "Hello "
+newStr = str + " World!"
+print (newStr) # Hello World!
+list1 = [1,2,3,4]
+listData = list1 + [5,61]
+print (listData) # [1, 2, 3, 4, 5, 6]
+
+tuplel = ("one", "two")
+tuple2 = ("three" ,)
+print (tuplel + tuple2) # ('one', 'two', 'three')
